5e45195d4408fd8ffd65b04c26b9421e319bfd6a
gamboj2 <jemil.gambo2@mail.dcu.ie>
2019-02-26 19:28:13 +0000

    login funtionality

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,7 +19,8 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('myapp/', include('myapp.urls'))
+    path('myapp/', include('myapp.urls')),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
 
 urlpatterns += staticfiles_urlpatterns()
