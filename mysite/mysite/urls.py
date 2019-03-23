335149ac4d37a381673716637fc10cb08de1d163
piratezilla <tralockrox@gmail.com>
2019-03-23 16:30:11 +0000

    urls.py blank to myapp

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,7 +19,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('myapp/', include('myapp.urls')),
+    path('', include('myapp.urls')),
     path('accounts/', include('django.contrib.auth.urls'))
 ]
 
