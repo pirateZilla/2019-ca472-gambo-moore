0067fb2c50ffeffb13aa49f2c0379a98039d8ec0
piratezilla <tralockrox@gmail.com>
2019-04-11 16:23:54 +0100

    Revert "email contact form testing"
    This reverts commit 5ce98eee47a4d21388390720af6740147ec30137.

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -8,30 +8,6 @@ import pprint
 import json
 from django.core.serializers.json import DjangoJSONEncoder
 
-from django.core.mail import send_mail, BadHeaderError
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render, redirect
-from myapp.forms import ContactForm
-
-def email(request):
-    if request.method == 'GET':
-        form = ContactForm()
-    else:
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            subject = form.cleaned_data['subject']
-            from_email = form.cleaned_data['from_email']
-            message = form.cleaned_data['message']
-            try:
-                send_mail(subject, message, from_email, ['tralockrox@gmail.com'])
-            except BadHeaderError:
-                return HttpResponse('Invalid header found.')
-            return redirect('thanks')
-    return render(request, "email.html", {'form': form})
-
-def thanks(request):
-    return HttpResponse('Thank you for your message.')
-
 # Create your views here.
 
 
